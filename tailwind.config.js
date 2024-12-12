a2e528dfe33d53d983890b6482ee9ae9d41d8153
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2024-12-12 16:23:00 +0545

    Tailwind config build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-/*eslint-env node*/
 import { nextui } from "@nextui-org/theme";
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
